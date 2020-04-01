768ac5bdcae7aaa1baf7996bb3595102ca13add0
ChandVachhani <18bce249@nirmauni.ac.in>
2020-04-01 18:02:46 +0530

    models

--- Alu_connect/user/models.py
+++ Alu_connect/user/models.py
@@ -10,7 +10,6 @@ class college(models.Model):
 
 class branch(models.Model):
     name = models.CharField(max_length=100)
-    college = models.ForeignKey(college,on_delete=models.CASCADE)
     class Meta:
         db_table='branch'
 
@@ -21,8 +20,11 @@ class roles(models.Model):
 
 class user_profile(models.Model):
     profile_img = models.ImageField()
+    college = models.OneToOneField(college, on_delete=models.CASCADE)
     branch = models.OneToOneField(branch,on_delete=models.CASCADE)
+    email = models.EmailField(max_length=254,on_delete=models.CASCADE)
     description = models.TextField()
+    #password
     user_role = models.ManyToManyField(roles,through='user_roles')
     class Meta:
         db_table='user_profile'
@@ -32,3 +34,68 @@ class user_roles(models.Model):
     user = models.OneToOneField(user_profile,on_delete=models.CASCADE)
     class Meta:
         db_table='user_roles'
+
+class tags(models.Model):
+    name = models.CharField(max_length=20)
+    class Meta:
+        db_table='tags'
+
+class projects(models.Model):
+    name = models.CharField(max_length=20)
+    # url = 
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    tag = models.ManyToManyField(tags,on_delete=models.CASCADE)
+    modified_date = models.DateField()
+    class Meta:
+        db_table='projects'
+
+class blogs(models.Model):
+    title = models.CharField(max_length=20)
+    author = models.OneToOneField(user_profile,on_delete=models.CASCADE)
+    content = models.TextField()
+    # views
+    class Meta:
+        db_table='blogs'
+
+class publications(models.Model):
+    Title = models.CharField(max_length=20)
+    Description = models.CharField(max_length=20)
+    # url
+    Tag = models.ManyToManyField(tags,on_delete=models.CASCADE)
+    class Meta:
+        db_table='publications'
+
+class skills(models.Model):
+    Skill = models.CharField(max_length=20)
+    class Meta:
+        db_table="skills"
+
+class connect(models.Model):
+    Junior = models.OneToOneField(user_profile,on_delete=models.CASCADE)
+    Alumni = models.OneToOneField(user_profile,on_delete=models.CASCADE)
+    class Meta:
+        db_table="connect"
+
+class company(models.Model):
+    name = models.CharField(max_length=20)
+    class Meta:
+        db_table="company"
+
+class InterviewExperience(models.Model):
+    companyName = models.ManyToManyField(company,on_delete=models.CASCADE)
+    class Meta:
+        db_table="InterviewExperience"
+
+class achievement(models.Model):
+    name = models.CharField(max_length=20)
+    #link
+    class Meta:
+        db_table="achievement"
+
+class OJList(models.Model):
+    ojName = models.CharField(max_length=20)
+    #
+    class Meta:
+        db_table="OJList"
+
