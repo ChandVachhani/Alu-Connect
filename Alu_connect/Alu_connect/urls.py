720b478dae47183d0a7a27d00d9f9a993e714b82
Devansh <devanshsuthar0612@gmail.com>
2020-04-16 14:02:48 +0530

    Small changes in UI.

--- Alu_connect/Alu_connect/urls.py
+++ Alu_connect/Alu_connect/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', include('user.urls'))
+    path('', include('user.urls'))
 ]
--- Alu_connect/user/views.py
+++ Alu_connect/user/views.py
@@ -8,6 +8,11 @@ from django.contrib.auth import login as login_user
 from django.contrib.auth import authenticate,password_validation
 # Create your views here.
 
+
+def main_page(request):
+    return render(request,'user/main.html')
+
+
 def login(request):
     if request.method == 'POST':
         login_form = LoginForm(request.POST)
@@ -27,7 +32,7 @@ def login(request):
     return render(request,'user/login.html',{'form':login_form})
 
 
-def SignUp(request):
+def SignUpStudent(request):
     if request.method == 'POST':
         signup_form = StudentSignUpForm(request.POST)
         if signup_form.is_valid():
@@ -41,12 +46,34 @@ def SignUp(request):
                 return redirect('login')
             except IntegrityError as error:
                 signup_form.add_error('student_username',error)
-            return render(request, 'user/signup.html', {'student_form': signup_form})
+            return render(request, 'user/signup_student.html', {'student_form': signup_form})
         else:
-            return render(request, 'user/signup.html', {'student_form': signup_form})
+            return render(request, 'user/signup_student.html', {'student_form': signup_form})
     else:
         signup_form = StudentSignUpForm()
-        return render(request,'user/signup.html',{'student_form':signup_form})
+        return render(request,'user/signup_student.html',{'student_form':signup_form})
+
+
+def SignUpAlumni(request):
+    if request.method == 'POST':
+        signup_form = StudentSignUpForm(request.POST)
+        if signup_form.is_valid():
+            first_name = signup_form.cleaned_data['student_first_name']
+            last_name = signup_form.cleaned_data['student_last_name']
+            username = signup_form.cleaned_data['student_username']
+            email = signup_form.cleaned_data['student_email']
+            password = signup_form.cleaned_data['student_password']
+            try:
+                student = User.objects.create_user(username=username,email=email,password=password,first_name=first_name,last_name=last_name)
+                return redirect('login')
+            except IntegrityError as error:
+                signup_form.add_error('student_username',error)
+            return render(request, 'user/signup_alumni.html', {'student_form': signup_form})
+        else:
+            return render(request, 'user/signup_alumni.html', {'student_form': signup_form})
+    else:
+        signup_form = StudentSignUpForm()
+        return render(request,'user/signup_alumni.html',{'student_form':signup_form})
 
 
 @login_required
