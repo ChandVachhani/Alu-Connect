e07169e55fd2b9a69cb1ec0cd8abaf99e8ff1ad1
ChandVachhani <18bce249@nirmauni.ac.in>
2020-04-16 15:17:47 +0530

    chand update

--- Alu_connect/user/forms.py
+++ Alu_connect/user/forms.py
@@ -52,3 +52,21 @@ class StudentSignUpForm(forms.Form):
         if required_domain not in email:
             raise forms.ValidationError("Only University E-mail is allowed!")
         return email
+
+
+class AddProjectForm(forms.Form):
+    project_name = forms.CharField(max_length=25, widget=forms.TextInput(attrs={
+        'class' : 'form-control',
+        'id' : 'project-title',
+        'style' : 'font-size: 18px !important;',
+    }))
+    project_Link = forms.CharField(max_length=50, widget=forms.TextInput(attrs={
+        'class' : 'form-control',
+        'id' : 'project-link',
+        'style' : 'font-size: 18px !important;',
+    }))
+    project_description = forms.CharField(max_length=300, widget=forms.TextInput(attrs={
+        'class' : 'form-control',
+        'id' : 'project-description',
+        'style' : 'font-size: 18px !important;',
+    }))
