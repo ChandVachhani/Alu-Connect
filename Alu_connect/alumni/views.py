bec33eebb783a19ad1b14d69e1c5fae2d816c3bc
ChandVachhani <18bce249@nirmauni.ac.in>
2020-04-17 16:30:42 +0530

    search algorithm for person

--- Alu_connect/alumni/views.py
+++ Alu_connect/alumni/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render,redirect
 from user.models import projects
+from django.contrib.auth.models import User
 from user.forms import AddProjectForm
 from django.db import IntegrityError
 from django.contrib.auth.decorators import login_required
+from .algorithms import lcs,sort
 
 
 # Create your views here.
@@ -27,3 +29,28 @@ def addproject(request):
     else:
         add_project_form = AddProjectForm()
         return render(request, 'alumni/main.html', {'add_project_form':add_project_form})
+
+
+def search_page(request):
+    if request.GET:
+        para_dict = request.GET
+        value = para_dict['search-result']
+        total_person = User.objects.all().order_by('Username')
+        search_count=0
+        search_list = []
+        max_match = 0
+        for person in total_person:
+            lcs_val = lcs(str(person.Username),value)
+            max_match = max(max_match,lcs_val)
+        for person in total_person:
+            person_tuple = (person,lcs(str(person.Username),value))
+            if(person_tuple[1]>int(max_match/2) and person_tuple[1]>=int(len(value))/2):
+                search_list.append(person_tuple)
+                products_count+=1
+        sort(search_list)
+        final_search_list=[]
+        for i in search_list:
+            final_search_list.append(i[0])
+        context_dict = {'result':final_search_list,'total_result':search_count,'search_result':value}
+        return render(request, 'alumni/main.html',context_dict)
+    return render(request,'alumni/main.html')
